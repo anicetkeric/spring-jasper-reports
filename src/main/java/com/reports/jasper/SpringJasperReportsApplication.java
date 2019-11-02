7eafa744c8aa65369ec83d4f22e87d8807c1aff7
aek <anicetkeric@gmail.com>
2019-11-02 20:43:08 +0000

    iitial commit

---
+++ src/main/java/com/reports/jasper/SpringJasperReportsApplication.java
@@ -0,0 +1,13 @@
+package com.reports.jasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJasperReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJasperReportsApplication.class, args);
+	}
+
+}
